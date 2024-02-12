b7ca00bd76a13ed47754071c1a9bd973d77b878e
Em Nam <78702559+Em-Nam@users.noreply.github.com>
2024-02-12 15:32:52 -0800

    Basic functionality of swapping css, rotation

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-console.log("Test");
 
 
+const sunMoonContainer = document.getElementsByClassName("sun-moon-container")[0];
 
-const always = () =>{
-    if (true) {
-        console.log("it is always true");
-    }
+document.getElementsByClassName("theme-toggle-button")[0].addEventListener("click", toggleTheme);
+
+function toggleTheme() {
+    document.body.classList.toggle("dark");
+    const currRotation = parseInt(getComputedStyle(sunMoonContainer).getPropertyValue('--rotation'));
+    sunMoonContainer.style.setProperty('--rotation', currRotation + 180);
+    
 }
-
-always();
