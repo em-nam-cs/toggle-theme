2c743abab9fb1ea4b93438bf77cdf58e3cb046ff
Em Nam <78702559+Em-Nam@users.noreply.github.com>
2024-02-12 16:26:17 -0800

    Swap Button Text to match modes

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
 
+const LIGHT_BUTTON_TEXT = "Swap to Dark Theme";
+const DARK_BUTTON_TEXT = "Swap to Light Theme";
 
+const FULL_REVOLUTION = 360;
+
+const BUTTON_TEXT = Array(DARK_BUTTON_TEXT, LIGHT_BUTTON_TEXT);
 const sunMoonContainer = document.getElementsByClassName("sun-moon-container")[0];
+const toggleBtn = document.getElementsByClassName("theme-toggle-button")[0];
 
-document.getElementsByClassName("theme-toggle-button")[0].addEventListener("click", toggleTheme);
+toggleBtn.addEventListener("click", toggleTheme);
+
+// currElement.innerHTML = BUTTON_TEXT[-1 * currState / 100]; //ON load put text
 
 function toggleTheme() {
-    document.body.classList.toggle("dark");
     const currRotation = parseInt(getComputedStyle(sunMoonContainer).getPropertyValue('--rotation'));
     sunMoonContainer.style.setProperty('--rotation', currRotation + 180);
-    
+
+    const currentlyDark = currRotation % FULL_REVOLUTION;  //0 if full rev (currently light), 180 if dark
+    const index = -1 * currentlyDark / 100;     //convert state to index (0 if switching to dark, -1.8 if switching to light)
+
+    // const delay = getComputedStyle(document.body).getPropertyValue('--transition-delay');
+    // console.log(delay);
+
+    for (i = 0; i < toggleBtn.children.length; i++) {
+        const currElement = toggleBtn.children[i]
+
+        //replace text in the btn's span
+        if (currElement.tagName == "SPAN") {
+            // console.log("START");
+            // setTimeout(() => {
+                currElement.innerHTML = BUTTON_TEXT.at(index);
+                // console.log("HERE");
+            // }, delay);
+
+        }
+    }
+
+    document.body.classList.toggle("dark");
+
 }
