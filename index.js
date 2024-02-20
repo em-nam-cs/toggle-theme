4e1f5aeba6241b6a07216d5a3965338d64dc50b7
Em Nam <78702559+Em-Nam@users.noreply.github.com>
2024-02-20 10:14:30 -0800

    Clean code, remove console logs, add documentation

--- index.js
+++ index.js
@@ -4,16 +4,11 @@
     - Switches the text on the button
     - Rotates the icons in clockwise circle
     - Changes CSS styling to add or remove the dark mode styles
-
     - Stores theme in local storage so refreshing keeps saved theme
 
 @author Em Nam
 @date 02/20/2024
  */
-
- /**
-  * issue with rotation if it does not start on the light mode
-  */
 
 const BUTTON_TEXT = {
     "LIGHT_TEXT": "Swap to Dark Theme",
@@ -27,12 +22,10 @@ const toggleBtnText = document.getElementById("toggle-button-text");
 const toggleIcon = document.getElementById("toggle");
 
 toggleBtn.addEventListener("click", toggleTheme);
-
 let darkTheme = localStorage.getItem("darkTheme");
 
-//Set toggle button text upon loading
+//Set toggle button text upon loading 
 document.addEventListener("DOMContentLoaded", () => {
-    console.log(`storage before init: ${localStorage.getItem("darkTheme")}`);
     if (darkTheme === "enabled") {
         setDarkTheme(0);
     } else {
@@ -40,28 +33,30 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 })
 
-
+/**
+ * sets the light theme text on toggle button, removes localStorage variable, 
+        and removes css dark class
+ * @param {int} delay delay of transition in ms
+ */
 function setLightTheme(delay) {
-    console.log("SEt light");
-    
-    console.log(`storage before set L: ${localStorage.getItem("darkTheme")}`);
     setTimeout(() => {
         toggleBtnText.innerHTML = BUTTON_TEXT["LIGHT_TEXT"];
         localStorage.setItem("darkTheme", null);
         document.body.classList.remove("dark"); 
-        console.log(`storage after set L: ${localStorage.getItem("darkTheme")}`);
     }, delay/2);    
 
 }
 
+/**
+ * sets the dark theme text on toggle button, localStorage variable, and css 
+        dark class
+ * @param {int} delay delay of transition in ms
+ */
 function setDarkTheme(delay) {
-    console.log("SEt dark");
-    console.log(`storage before set D: ${localStorage.getItem("darkTheme")}`);
     setTimeout(() => {
         toggleBtnText.innerHTML = BUTTON_TEXT["DARK_TEXT"];
         localStorage.setItem("darkTheme", "enabled");
         document.body.classList.add("dark"); 
-        console.log(`storage before set D: ${localStorage.getItem("darkTheme")}`);
     }, delay/2);    
 }
 
@@ -72,11 +67,9 @@ function setDarkTheme(delay) {
  */
 function toggleTheme() {
 
+    //sets new rotation value by adding half rev so always moves in cw direction
     const currRotation = parseInt(getComputedStyle(sunMoonContainer).getPropertyValue('--rotation'));
-    sunMoonContainer.style.setProperty('--rotation', currRotation + 180);
-    // const currentlyDark = currRotation % FULL_REVOLUTION;  //0 if full rev (currently light), 180 if dark
-
-    console.log(`rotation: ${currRotation}`);
+    sunMoonContainer.style.setProperty('--rotation', currRotation + FULL_REVOLUTION/2);
 
     let delay = getComputedStyle(document.body).getPropertyValue('--animiation-transition');
     delay = delay.replace(/\D/g,'');     //convert to ms int
@@ -92,14 +85,10 @@ function toggleTheme() {
     //switch button text when faded (div 2 because that's when opacity 0 (at 50%, halfway through transition))
     console.log(`storage before cond: ${localStorage.getItem("darkTheme")}`);
     darkTheme = localStorage.getItem("darkTheme");
-    if (darkTheme === "enabled") {  //make this so it makes better sense and matches start up if()
+    if (darkTheme === "enabled") {
         setLightTheme(delay);
     } else {
         setDarkTheme(delay);
     } 
-
-    // document.body.classList.toggle("dark"); 
-    //I want this outsid eof the timeout, but I also want it in the helper function so I can
-    //set the mode on init and appropriate styles
 
 }
